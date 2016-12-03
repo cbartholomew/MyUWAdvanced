4bdc09b79e9b92d5df762b93d3161513a371547e
Christopher Bartholomew <c_bartholomew@hotmail.com>
2016-12-02 21:09:03 -0800

    updated

--- WinAdvance/Settings/ApplicationSettings.Designer.cs
+++ WinAdvance/Settings/ApplicationSettings.Designer.cs
@@ -73,7 +73,7 @@ namespace WinAdvance.Settings {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("E:\\Output.csv")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\Christopher\\Desktop\\Temp\\Output.csv")]
         public string FILE_EXPORT_PATH {
             get {
                 return ((string)(this["FILE_EXPORT_PATH"]));
@@ -85,7 +85,7 @@ namespace WinAdvance.Settings {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("278")]
+        [global::System.Configuration.DefaultSettingValueAttribute("368")]
         public int START_AT_ROW {
             get {
                 return ((int)(this["START_AT_ROW"]));
